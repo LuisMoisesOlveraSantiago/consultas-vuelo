c550dea40cc5cbf562b6aee0aa91f935ce978790
LuisMoisesOlveraSantiago <luismoises.olverasantiago.gen23@cbtis105.edu.mx>
2025-05-12 08:20:43 -0600

    inicial

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ConsultasVuelo.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
